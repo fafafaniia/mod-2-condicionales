2cf987d851e71e24d4ff5238ec8ce5f66f760207
Estefanía <fafafaniia95@gmail.com>
2024-12-03 19:01:16 +0100

    new exercise

--- js/main.js
+++ js/main.js
@@ -34,29 +34,39 @@ if (number===0) {
   } else { 
     console.log('el número no es 123123125')
   }
-'use strict';
-const access = document.querySelector('.js_access')
-const nameAccess = 'Maria';
-const nameAccess2 = 'Luisa';
-const nameParagraph = document.querySelector('.js_nameParagraph')
-const btn = document.querySelector('.js_btn')
+
+  //Ejercicio 3 Nadie sin avatar
+
+  // avatar por defecto
+const DEFAULT_AVATAR = "http://placehold.jp/150x150.png";
+// avatar que eligió el usuario al registrarse
+//let userAvatar = '';
+let userAvatar ='https://dev.adalab.es/gato-siames.webp'
 
 
-  
-        btn.addEventListener('click', (ev) => {
-            ev.preventDefault()
-            const selectName = access.value;
-            nameParagraph.innerHTML = selectName;
-            if (selectName === nameAccess || selectName === nameAccess2) {
-                nameParagraph.innerHTML = 'Acceso permitido';
-            
-            }
-            else {
-                nameParagraph.innerHTML = 'Acceso no permitido';
-            }
-        } ) 
+const usAv = document.querySelector('.user__avatar') ;
 
+usAv.src = userAvatar || DEFAULT_AVATAR;
 
+// Ejercicio 4. Conversor de edad de perro a humano 
+const convertButton = document.querySelector('convertButton');
+const dogAgeInput = document.querySelector('dogAge');
+const resultParagraph = document.querySelector('result');
 
+convertButton.addEventListener('click', (ev) => {
+  ev.preventDefault()
+  const edadPerro = parseInt(dogAgeInput.value);
+  if (isNaN(edadPerro) || edadPerro <= 0) {
+    resultParagraph.innerHTML = "Por favor, introduce una edad válida.";}
+})
 
+let edadHumana;
 
+if (edadPerro === 1) {
+    edadHumana = 15;
+} else if (edadPerro === 2) {
+    edadHumana = 15 + 9; // Primer año + segundo año
+} else if (edadPerro >= 3) {
+    // Primer año = 15 años, segundo año = 9 años, el resto = 5 años por cada año adicional
+    edadHumana = 15 + 9 + (edadPerro - 2) * 5;
+}
