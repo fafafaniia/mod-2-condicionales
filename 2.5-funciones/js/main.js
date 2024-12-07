fae24d6773a156df351bdbebf527e6e0a41ddb1b
Julia <julialopezcalvo95@gmail.com>
2024-12-07 13:00:09 +0100

    ejercicios sabado 7 funcioones

--- 2.5-funciones/js/main.js
+++ 2.5-funciones/js/main.js
@@ -25,3 +25,27 @@ function pair(a) {
   console.log(pair (7));
 
 
+//Ticket con IVA
+
+function price(a) {
+  const iva = a * 0.21;
+  const total = a + iva;
+  const result = `Precio sin IVA: ${a}, IVA: ${iva.toFixed(2)} y Total: ${total.toFixed(2)}`;
+
+  return result; // Devolvemos el texto generado
+  //Se usa toFixed(2) para limitar los valores decimales a dos dígitos.
+
+}
+
+
+const priceResult = price(10); // Llamamos a la función con un valor de prueba
+console.log('Texto:', priceResult); // Imprimimos el resultado
+
+
+//Ejercicio Extra --> querySelector
+const btnEl = getEl('.btn');
+
+function getEl (selector) {
+  return document.querySelector (selector);
+}
+btnEl.addEventListener('click', () => { alert('Button clicked!'); });
