eea49bd7c3f139e2275741a7d75428a28542e24b
Estefanía <fafafaniia95@gmail.com>
2024-12-10 21:41:34 +0100

    ejercicio objetos

--- 2.5-funciones/js/main.js
+++ 2.5-funciones/js/main.js
@@ -25,3 +25,28 @@ function pair(a) {
   console.log(pair (7));
 
 
+function price(a) {
+    const iva = a * 0.21;
+    const total = a + iva;
+    return result;
+  }
+
+  const adalaber1 = {
+    name: 'Susana',
+    age: 34,
+    profession: 'periodista',
+  }
+
+  const adalaber2 = {
+    name: 'Rocío',
+    age: 25,
+    profession: 'actriz',
+  }
+
+ const parrafo = document.querySelector('.js_adalaber1');
+ parrafo.innerHTML = `Mi nombre es ${adalaber1.name}, tengo ${adalaber1.age} y soy ${adalaber1.profession}.`
+  
+ console.log(adalaber1)
+
+
+
